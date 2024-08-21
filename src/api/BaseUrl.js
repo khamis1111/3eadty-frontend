57ce0bc2944df8d18b1372915d4d075f01713108
unknown <mahmood@gmail.com>
2024-08-21 16:15:15 +0300

    add backendUrl

--- src/api/BaseUrl.js
+++ src/api/BaseUrl.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-export const urlWeb = "http://127.0.0.1:8000";
-// export const urlWeb = "https://portfolio-yaya-backend.vercel.app";
-const baseUrl = axios.create({ baseURL: urlWeb });
+
+const baseUrl = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 
 export default baseUrl;
